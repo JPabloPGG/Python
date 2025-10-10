5289a2a609d9618189cc5c1e8ca00a41a62c325c
Juan <pablopuentes.gomez@gmail.com>
2025-10-09 20:41:11 -0500

    Add files via upload

---
+++ Productos.py
@@ -0,0 +1,9 @@
+producto = ("Cámara digital")
+precio = 3999,999
+cantidad = 20
+disponible = True
+print ("***Sistema de Tienda Online***")
+print ("Producto: ", producto)
+print ("Precio: ", precio)
+print ("Cantidad Inventario: ", cantidad)
+print ("Disponible: ", disponible)
