1c22a0a8111490642c45449d0fb061ca0830c976
Juan <pablopuentes.gomez@gmail.com>
2025-10-09 21:34:13 -0500

    Add files via upload

---
+++ indexa.py
@@ -0,0 +1,42 @@
+##cadena = "Python"
+##print (cadena[0])
+##print (cadena[3])
+##print (cadena[-1])
+##print (cadena[-2])
+
+##cadena = "Python"
+##[inicio:fin:paso]
+##print (cadena[1:4])
+##print (cadena[:3])
+##print (cadena[::2])
+
+##Ejercicio 1: 
+cadena1 = "PythonProgramming"
+print (cadena1[0])
+print (cadena1[1])
+print (cadena1[2])
+print (cadena1[3])
+print (cadena1[4])
+print (cadena1[5])
+
+print (cadena1[9])
+print (cadena1[10])
+print (cadena1[11])
+print (cadena1[12])
+print (cadena1[13])
+print (cadena1[14])
+print (cadena1[15])
+print (cadena1[16])
+
+print (cadena1[2])
+print (cadena1[3])
+print (cadena1[4])
+print (cadena1[5])
+print (cadena1[6])
+print (cadena1[7])
+print (cadena1[8])
+##Ejercicio 2:
+cadena2 = "Rebanado"
+print (cadena2[::-1])
+
+print (cadena1[9:17])
